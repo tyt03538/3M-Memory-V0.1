b84e1e41ec9dff1c73f3f381e6572825bbc32908
user <user@users-macbook-pro.local>
2016-09-06 17:24:16 +0800

    Initial Version

---
+++ 3M-Memory-v0.1/FlightListVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  3M-Memory-v0.1
+//
+//  Created by user on 6/9/2016.
+//  Copyright © 2016 CPA. All rights reserved.
+//
+
+import UIKit
+
+class FlightListVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func BackButton(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion:nil)
+    }
+}
